906820c36c400d31470c7c73d8876f3f9247ed73
Matthew Hatcher <matthewh@halfpetal.com>
2021-05-10 12:30:57 -0500

    Adding utilities class and platform enum

---
+++ src/Enumerations/Platform.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paladins.Net.Enumerations
+{
+    public enum Platform
+    {
+        Unknown = 0,
+        HiRez = 1,
+        Steam = 5,
+        EpicGames = 28,
+        PSN = 9,
+        XboxLive = 10,
+        Nintendo = 22,
+        Discord = 25,
+        Mixer = 14,
+        Facebook = 12,
+        Google = 13,
+    }
+}
---
+++ src/Utilities.cs
@@ -0,0 +1,56 @@
+﻿using Paladins.Net.Enumerations;
+
+namespace Paladins.Net
+{
+    public class Utilities
+    {
+        public static Platform ParsePlatform(string platform)
+        {
+            switch (platform.ToLower())
+            {
+                case "pc":
+                case "hirez":
+                case "standalone":
+                    return Platform.HiRez;
+                case "steam":
+                case "s":
+                    return Platform.Steam;
+                case "epic":
+                case "egs":
+                case "epicgames":
+                case "eg":
+                    return Platform.EpicGames;
+                case "psn":
+                case "playstation":
+                case "ps4":
+                case "ps5":
+                case "ps":
+                    return Platform.PSN;
+                case "xbox":
+                case "xboxlive":
+                case "xbl":
+                case "xbox1":
+                case "xbox_one":
+                case "xboxone":
+                case "xb":
+                    return Platform.XboxLive;
+                case "nintendo":
+                case "switch":
+                    return Platform.Nintendo;
+                case "discord":
+                case "d":
+                    return Platform.Discord;
+                case "mixer":
+                    return Platform.Mixer;
+                case "facebook":
+                case "fb":
+                    return Platform.Facebook;
+                case "google":
+                case "gmail":
+                    return Platform.Google;
+                default:
+                    return Platform.Unknown;
+            }
+        }
+    }
+}
