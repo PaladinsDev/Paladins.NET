1de1eb86c423bc6313c6d8ca17b0e606639ff0c9
Matthew Hatcher <matthewh@halfpetal.com>
2021-05-10 11:03:33 -0500

    Adding some base API constructor information.

--- src/API.cs
+++ src/API.cs
@@ -2,5 +2,15 @@
 {
     public class API
     {
+        private string _devID;
+        private string _authKey;
+        private bool _debugMode;
+
+        public API(string DevID, string AuthKey, bool DebugMode = false)
+        {
+            this._devID = DevID;
+            this._authKey = AuthKey;
+            this._debugMode = DebugMode;
+        }
     }
 }
