d80defa06bb252975b0508d4ade62464e777cd74
Matthew Hatcher <matthewh@halfpetal.com>
2021-05-10 22:24:33 -0500

    Adding some exceptions.

---
+++ src/Exceptions/Exception.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Paladins.Net.Exceptions
+{
+    public class PrivateProfile : Exception { }
+    public class NotFound : Exception { }
+    public class PlayerNotFound : NotFound { }
+    public class Unauthorized : Exception { }
+    public class Unavailable : Exception { }
+    public class DailyLimitReached : Exception { }
+}
