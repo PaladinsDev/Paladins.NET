2881328d607a06a3b971f3f96a790341d22f7418
Matthew Hatcher <matthewh@halfpetal.com>
2021-05-11 11:45:39 -0500

    Adding data usage model.

---
+++ src/Models/System.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paladins.Net.Models
+{
+    public class DataUsage : IModel
+    {
+        public string Timestamp { get; set; }
+        public uint ActiveSessions { get; set; }
+        public uint ActiveSessionsLimit { get; set; }
+        public uint SessionsUsed { get; set; }
+        public uint SessionsLimit { get; set; }
+        public int SessionLifetime { get; set; }
+        public uint RequestsUsed { get; set; }
+        public uint RequestsLimit { get; set; }
+    }
+}
