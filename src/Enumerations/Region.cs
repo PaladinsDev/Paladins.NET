830c06029b3073d545f4997c793678c54aa056a4
Matthew Hatcher <matthewh@halfpetal.com>
2021-05-10 23:04:30 -0500

    Adding regions.

---
+++ src/Enumerations/Region.cs
@@ -0,0 +1,14 @@
+﻿namespace Paladins.Net.Enumerations
+{
+    public enum Region
+    {
+        Unknown,
+        North_America,
+        Latin_America_North,
+        Brazil,
+        Europe,
+        Southest_Asia,
+        Japan,
+        Australia,
+    }
+}
