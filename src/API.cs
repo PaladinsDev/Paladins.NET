d6d722ea4c566f302ca99d8e8bd6e9baabd86b60
Matthew Hatcher <matthewh@halfpetal.com>
2021-05-18 23:33:56 -0500

    Adding url and timestamp

--- src/API.cs
+++ src/API.cs
@@ -1,16 +1,20 @@
-﻿namespace Paladins.Net
+namespace Paladins.Net
 {
     public class API
     {
         private string _devID;
         private string _authKey;
         private bool _debugMode;
+        private string _timestamp;
+        private string _urlBase;
 
-        public API(string DevID, string AuthKey, bool DebugMode = false)
+        public API(string DevID, string AuthKey, bool DebugMode = false, string apiUrl = "https://api.paladins.com/paladinsapi.svc")
         {
             this._devID = DevID;
             this._authKey = AuthKey;
             this._debugMode = DebugMode;
+            this._timestamp = System.DateTime.UtcNow.ToString("yyyyMMddHHmmss");
+            this._urlBase = apiUrl;
         }
     }
 }
