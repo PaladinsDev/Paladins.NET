90faf3719d9f05e8005d9d027b69b3a92d4a2185
Matthew Hatcher <matthewh@halfpetal.com>
2021-07-11 00:59:39 -0500

    Adding auth model.

--- Paladins.Net.Tests/ApiFixture.cs
+++ Paladins.Net.Tests/ApiFixture.cs
@@ -1,6 +1,8 @@
 ﻿namespace Paladins.Net.Tests
 {
     using System;
+    using System.IO;
+    using System.Text.Json;
 
     /// <summary>
     /// Class fixture to persist instances of the API to prevent hitting API and session limits.
@@ -12,8 +14,14 @@
         /// </summary>
         public ApiFixture()
         {
-            //
+            string authFile = File.ReadAllText("config\\auth.json");
+            this.Auth = JsonSerializer.Deserialize<AuthModel>(authFile);
         }
+
+        /// <summary>
+        /// Gets or sets auth.
+        /// </summary>
+        public AuthModel Auth { get; set; }
 
         /// <summary>
         /// Cleanup.
---
+++ Paladins.Net.Tests/AuthModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Paladins.Net.Tests
+{
+    /// <summary>
+    /// Auth Model.
+    /// </summary>
+    public class AuthModel
+    {
+        /// <summary>
+        /// Gets or sets Auth Key.
+        /// </summary>
+        public string AUTH_KEY { get; set; }
+
+        /// <summary>
+        /// Gets or sets Dev ID.
+        /// </summary>
+        public string DEV_ID { get; set; }
+
+    }
+}
