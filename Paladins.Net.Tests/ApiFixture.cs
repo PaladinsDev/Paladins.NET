169fdd64e0c92974aff56ad9bacb8c80765413b1
Matthew Hatcher <matthewh@halfpetal.com>
2021-07-11 00:25:55 -0500

    Adding fixture and JSON API test class.

---
+++ Paladins.Net.Tests/ApiFixture.cs
@@ -0,0 +1,26 @@
+﻿namespace Paladins.Net.Tests
+{
+    using System;
+
+    /// <summary>
+    /// Class fixture to persist instances of the API to prevent hitting API and session limits.
+    /// </summary>
+    public sealed class ApiFixture : IDisposable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiFixture"/> class.
+        /// </summary>
+        public ApiFixture()
+        {
+            //
+        }
+
+        /// <summary>
+        /// Cleanup.
+        /// </summary>
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Paladins.Net.Tests/TestJsonApi.cs
@@ -0,0 +1,24 @@
+﻿namespace Paladins.Net.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Paladins.Net.Enumerations;
+    using Paladins.Net.Models;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// JSON API class tests.
+    /// </summary>
+    public class TestJsonApi
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void TestCreateSession()
+        {
+            // Method intentionally left empty.
+        }
+    }
+}
